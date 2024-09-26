b10ec9ec75409258f688d1a8eda14c1f8dec18d2
NOOMA-42 <tzuchun42@gmail.com>
2024-09-26 16:37:44 +0800

    sync: oracle query unique line

--- src/common_util/test/sumcheck_test.py
+++ src/common_util/test/sumcheck_test.py
@@ -1,11 +1,12 @@
 import unittest
-from src.common_util.sumcheck import prove_sumcheck, verify_sumcheck
+from src.common_util.sumcheck import prove_sumcheck, verify_sumcheck, reduce_multiple_polynomial
 from src.common_util.curve import Scalar
 from src.common_util.mle_poly import (
     polynomial, term, monomial, 
-    generate_binary, eval_ext, get_multi_ext, eval_expansion
+    get_multi_ext, 
+    eval_univariate,
 )
-from src.logupgkr.transcript import Transcript # change to common transcript
+from src.logupgkr.transcript import Transcript # TODO change to common transcript
 
 def test_polynomial():
     a = Scalar(2)
@@ -36,6 +37,15 @@ def test_polynomial2():
     monomial1 = monomial(coeff=d, terms=[term1, term2])  # 5 * ((2 * x_1 + 1) * (3 * x_2 + 4))
     # you can't have extra const term in this polynomial
     poly = polynomial(terms=[monomial1]) # 5 * ((2 * x_1 + 1) * (3 * x_2 + 4))
+    return poly
+
+def test_polynomial3():
+    a = Scalar(2)
+    d = Scalar(5)
+    e = Scalar(1)
+    term1 = term(coeff=a, i=1, const=e)  # (2 * x_1 + 1)
+    monomial1 = monomial(coeff=d, terms=[term1])  # 5 * (2 * x_1 + 1)
+    poly = polynomial(terms=[monomial1]) # 5 * (2 * x_1 + 1)
     return poly
 
 def test_polynomial_function2(values):
@@ -85,6 +95,27 @@ class TestSumcheck(unittest.TestCase):
                 raise ValueError("Invalid input")
         print(get_multi_ext(D_func, 1))
 
+    def test_reduce_multiple_polynomial(self):
+        """  
+        proof argument zero knowledge 4.5.2 
+        """
+        term1 = term(coeff=Scalar(3), i=1, const=Scalar(0))  # 3 * x_1
+        term2 = term(coeff=Scalar(1), i=2, const=Scalar(0))  # x_2
+        term3 = term(coeff=Scalar(2), i=2, const=Scalar(0))  # 3 * x_2
+        
+        monomial1 = monomial(coeff=Scalar(1), terms=[term1, term2])  # 3 * x_1 * x_2
+        monomial2 = monomial(coeff=Scalar(1), terms=[term3])  # 3 * x_2
+        w = polynomial(terms=[monomial1, monomial2], c=Scalar(0))
+        b = [Scalar(2), Scalar(4)]
+        c = [Scalar(3), Scalar(2)]
+
+        l = reduce_multiple_polynomial(b, c, w)
+        l_one = eval_univariate(l, one)
+        l_zero = eval_univariate(l, zero)
+        
+        assert l_one == w.evaluate(c)
+        assert l_zero == w.evaluate(b)
+
     def test_prove_sumcheck(self):
         transcript = Transcript(b"test_sumcheck")
         f = test_polynomial2()
@@ -92,7 +123,21 @@ class TestSumcheck(unittest.TestCase):
 
     def test_verify_sumcheck(self):
         transcript = Transcript(b"test_sumcheck")
+        f = test_polynomial()
+        proof = prove_sumcheck(f, transcript)        
+        transcript2 = Transcript(b"test_sumcheck")
+        self.assertTrue(verify_sumcheck(proof, transcript2, f), "Verification failed")
+    
+    def test_verify_sumcheck2(self):
+        transcript = Transcript(b"test_sumcheck")
         f = test_polynomial2()
+        proof = prove_sumcheck(f, transcript)        
+        transcript2 = Transcript(b"test_sumcheck")
+        self.assertTrue(verify_sumcheck(proof, transcript2, f), "Verification failed")
+
+    def test_verify_sumcheck3(self):
+        transcript = Transcript(b"test_sumcheck")
+        f = test_polynomial3()
         proof = prove_sumcheck(f, transcript)        
         transcript2 = Transcript(b"test_sumcheck")
         self.assertTrue(verify_sumcheck(proof, transcript2, f), "Verification failed")
